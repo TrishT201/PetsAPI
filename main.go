a4fbf357bbda20c3d7c0f6cc00797c73c2972195
trish <trish.tran201@gmail.com>
2023-02-04 18:17:10 -0600

    add folders and load env variable

--- main.go
+++ main.go
@@ -16,18 +16,25 @@ package main
 	mongoDB - a document-based db management program used as an alternative to relational db.
 */
 
-import "github.com/gin-gonic/gin"
+// import dependencies
+import (
+	"github.com/gin-gonic/gin"
+)
 
-// initalize a gin router
 func main() {
+	// initialize a Gin router. THe Default configures Gin router w/ default middlewares (logger and recovery)
 	router := gin.Default()
 
+	// .Get is used to defined a handler for a GET request. It takes in route(/) and one or more route handlers as parameters.
 	router.GET("/", func(c *gin.Context) {
+
+		// JSON(code int, obj interface{}) renders the given obj as json
 		c.JSON(200, gin.H{
 			"data": "Hello from Gin-gonic & mongoDB",
 		})
 	})
 
+	// Listen and serve HTTP request on localhost:6000. By default it serves on :8080 unless defined
 	router.Run("localhost:6000")
 
 }
@@ -35,7 +42,7 @@ func main() {
 type pet struct {
 	ID    string  `json:"id"`
 	PName string  `json:"pname"`
-	DOB   float32 `json:"dob"`
+	DOB   float64 `json:"dob"`
 	Owner string  `json:"owner"`
 	Kind  string  `json:"kind"`
 	Size  int     `json:"size"`
