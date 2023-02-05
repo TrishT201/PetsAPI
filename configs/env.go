cc4d4bc1ae98a7e76a85aaf708c422acb13d4051
trish <trish.tran201@gmail.com>
2023-02-04 18:40:11 -0600

    load env variable

--- configs/env.go
+++ configs/env.go
@@ -1,5 +1,6 @@
 package configs
 
+// import dependencies
 import (
 	"log"
 	"os"
@@ -7,11 +8,17 @@ import (
 	"github.com/joho/godotenv"
 )
 
+// EnvMongoUri checks if the env variable is correctly loaded and return env variable
+// helper function to load the env variable using the godotenv library
 func EnvMongoURI() string {
+
+	// loads .env file in the current directory
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
+	// Getenv takes the name of the env variable("MONGOURI") and returns its associated. If the environment
+	// variable specified does not exist on the system, the function returns an empty value.
 	return os.Getenv("MONGOURI")
 }
