1a65e766bc16b342c939c20f6ddca7c7b08ddae6
Trish Tran <trish.tran201@gmail.com>
2023-03-10 02:41:24 -0600

    added SetDefault() to set values for Pet.Kind.

--- models/model.go
+++ models/model.go
@@ -19,20 +19,26 @@ We use them to reformat the JSON response returned by the API
 type Pet struct {
 	// primitive.ObjectID inform the MongoDB server that it's a MongoDB ObjectIds
 	ID    primitive.ObjectID
-	PName string `json:"pname"`
-	DOB   string `json:"dob"`
-	Owner string `json:"owner"`
-	Kind  Kind   `json:"kind"`
-	Size  Size   `json:"size"`
-	Toy   string `json:"toy"`
+	PName string   `json:"pname"`
+	DOB   string   `json:"dob"`
+	Owner string   `json:"owner"`
+	Kind  Kind     `json:"kind"`
+	Size  Size     `json:"size"`
+	Toy   []string `json:"favorite_toys"`
 }
 
 type Size struct {
-	Height string `json:"toy"`
+	Height string `json:"height"`
 	Weight string `json:"weight"`
 }
 
 type Kind struct {
 	Animal string `json:"animal"`
 	Breed  string `json:"breed"`
+}
+
+func (p *Pet) SetDefaults() {
+	if p.Kind.Animal == "dog" || p.Kind.Breed == "" {
+		p.Kind.Breed = "unknown"
+	}
 }
